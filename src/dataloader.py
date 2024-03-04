12f4d7ecf57da197912e14af63a9720974bc8f27
Peter Skrinjar <skrinj0000@worker02.cluster.bc2.ch>
2024-03-04 13:29:27 +0100

    adding sasa to node features

--- src/dataloader.py
+++ src/dataloader.py
@@ -14,6 +14,10 @@ import pickle
 import torch
 import lightning as L
 from torch.utils.data import random_split
+from Bio.PDB.mmtf import MMTFParser
+from Bio.PDB.SASA import ShrakeRupley
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 def load_graph(pdb_id, chain):
@@ -23,6 +27,16 @@ def load_graph(pdb_id, chain):
                                        partial(graphein_edges.add_distance_threshold, threshold=8., long_interaction_threshold=2)],
     )
     graph = construct_graph(pdb_code=pdb_id, config=graph_config, verbose=False)
+
+    struct = MMTFParser.get_structure_from_url(pdb_id)
+    sr = ShrakeRupley()
+    sr.compute(struct, level="R") #residue level
+     
+    for node, data in graph.nodes(data=True):
+        chain_id = data['chain_id']
+        residue_number = data['residue_number']
+        data['sasa'] = round(struct[0][chain_id][residue_number].sasa, 2)
+
     interface_residues = set()
     for source, target, kind in graph.edges(data=True):
         c1, c2 = source.split(":")[0], target.split(":")[0]
@@ -43,7 +57,10 @@ def graphein_to_torch_graph(graphein_graph, interface_labels, convertor,
     data_dict= data.to_dict()
     x_data = []
     for x in node_attr_columns:
-        x_data.append(torch.atleast_2d(data_dict[x]))
+        if data_dict[x].ndim == 1:  #if we want to include solvent accesible area, which is one-dimensional
+            x_data.append(torch.atleast_2d(data_dict[x]).T)
+        else:
+            x_data.append(torch.atleast_2d(data_dict[x]))
     data.x = torch.hstack(x_data).float()
     data.pos = data.coords.float()
     data.y = torch.zeros(data.num_nodes)
@@ -57,34 +74,24 @@ class ProteinDataset(Dataset):
     torch-geometric Dataset class for loading protein files as graphs.
     """
     def __init__(self, root,
-                 protein_names: list, 
+                 protein_names: list,
+                 columns: list, 
                  pre_transform=None, 
                  transform=None):
-        columns = [
-            "chain_id",
-            "coords",
-            "edge_index",
-            "kind",
-            "node_id",
-            "residue_number",
-            "amino_acid_one_hot",
-            "meiler"
-        ]
+
         self.convertor = GraphFormatConvertor(src_format="nx", dst_format="pyg", columns=columns, verbose = None)
         self.protein_names = protein_names
         super(ProteinDataset, self).__init__(root, pre_transform=pre_transform, transform=transform)
 
     def download(self):
         for protein_name in self.protein_names:
-            print(protein_name)
             output = Path(self.raw_dir) / f'{protein_name}.pkl'
             if not output.exists():
                 try:
                     pdb_id, chain = protein_name.split("_")
-                    graphein_graph, interface_labels = dataloader.load_graph(pdb_id, chain)
+                    graphein_graph, interface_labels = load_graph(pdb_id, chain)
                     with open(output, "wb") as f:
                         pickle.dump((graphein_graph, interface_labels), f)
-                    successful_protein_names.append(protein_name)
                 except Exception as e:
                     print(f"Cannot load {protein_name} because of {e}")
 
@@ -119,7 +126,7 @@ class ProteinDataset(Dataset):
         return data
 
 class ProteinGraphDataModule(L.LightningDataModule):
-    def __init__(self, root, dataset_file, batch_size=8, num_workers=4, pre_transform=None, transform=None):
+    def __init__(self, root, columns, dataset_file, batch_size=8, num_workers=4, pre_transform=None, transform=None):
         super().__init__()
         self.root = root
         self.dataset_file = dataset_file
@@ -130,6 +137,7 @@ class ProteinGraphDataModule(L.LightningDataModule):
         self.protein_names = self.protein_names[:64]
         self.pre_transform = pre_transform
         self.transform = transform
+        self.columns = columns
 
     def prepare_data(self):
         # download, IO, etc. Useful with shared filesystems
@@ -137,6 +145,7 @@ class ProteinGraphDataModule(L.LightningDataModule):
         # does the downloading and saving of graphein graphs part, just once
         ProteinDataset(root=self.root,
                         protein_names=self.protein_names,
+                        columns=self.columns,
                         pre_transform=self.pre_transform, 
                         transform=self.transform)
     
@@ -147,6 +156,7 @@ class ProteinGraphDataModule(L.LightningDataModule):
         # now it's just loaded and not downloaded processed etc.
         dataset = ProteinDataset(root=self.root,
                                   protein_names=self.protein_names,
+                                  columns=self.columns,
                                   pre_transform=self.pre_transform, 
                                   transform=self.transform)
         train_idx, val_idx, test_idx = random_split(range(len(dataset)), [0.8, 0.1, 0.1])
--- src/train.py
+++ src/train.py
@@ -1,8 +1,13 @@
 import dataloader, models
 from lightning.pytorch.cli import LightningCLI
+from loguru import logger
 
 
 def main():
+    """
+    Run with python train.py fit -c config.yaml
+    Or in an sbatch script with srun python main.py fit -c config.yaml
+    """
     LightningCLI(models.GATModule, dataloader.ProteinGraphDataModule)
 
 if __name__ == "__main__":
